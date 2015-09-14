73fc06830ce2cae9908dd0aafc0f9ad3e9ddd20a
Nico <ntresoldi@remedia.it>
2015-09-14 15:51:22 +0200

    First commit, playable game, miss statistics and restart button

---
+++ core/src/com/kilobolt/zbHelpers/InputHandler.java
@@ -0,0 +1,60 @@
+package com.kilobolt.zbHelpers;
+
+import com.badlogic.gdx.InputProcessor;
+import com.kilobolt.gameobjects.Bird;
+import com.kilobolt.gameworld.GameWorld;
+
+/**
+ * Created by nico on 01/09/15.
+ */
+public class InputHandler implements InputProcessor {
+    private GameWorld myWorld;
+    private Bird myBird;
+
+    public InputHandler(GameWorld myWorld) {
+        // myBird now represents the gameWorld's bird.
+        this.myWorld = myWorld;
+        myBird = myWorld.getBird();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        myBird.onClick();
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
